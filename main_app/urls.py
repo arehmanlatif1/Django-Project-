1648f1d8041da9d5fe54aadf517d92a844d8c148
arehmanlatif1 <rehmanlatif335@gmail.com>
2024-04-02 16:14:30 -0400

    add routes and make models

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import Home
+from .views import Home, BirdList, BirdDetail
 
 urlpatterns = [
     path("", Home.as_view(), name="home"),
+    path("birds/", BirdList.as_view(), name="bird-list"),
+    path("birds/<int:id>/", BirdDetail.as_view(), name="birddetail"),
+
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,19 @@
-from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import generics
+from .models import Bird
+from .serializers import BirdSerializer
 # Create your views here.
 
 class Home(APIView):
     def get(self, request):
         content = {"message": "Welcome to bird-Collector API home route!"}
         return Response(content)
+class BirdList(generics.ListCreateAPIView):
+    queryset = Bird.objects.all()
+    serializer_class = BirdSerializer
+
+class BirdDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Bird.objects.all()
+    serializer_class = BirdSerializer
+    lookup_field = 'id'
